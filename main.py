889a391034d5a1a37b43cd505d728d768ebe2e9c
Andreas Brommund <andreas@brommund.se>
2017-11-28 00:51:29 +0100

    Update test

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import test
 
 
 def main():
-
     test.test_extended_euclidean_algorithm()
     test.test_gcd()
     test.test_power_mod()
     test.test_int_to_list_and_list_to_int()
-    # test.test_rabin_cipher()
+    test.test_rabin_cipher()
     print("Done")
-
 
 if __name__ == '__main__':
     main()
