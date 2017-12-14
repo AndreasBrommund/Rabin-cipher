a13b9e95dcb2bd932e832f9e198c5b35a6964aad
Andreas Brommund <andreas@brommund.se>
2017-12-14 13:08:08 +0100

    Better test control

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import test
+from cipher import SimpleCipher
 
 
 def main():
-    test.test_extended_euclidean_algorithm()
-    test.test_power_mod()
-    test.test_int_to_list_and_list_to_int()
-    test.test_rabin_cipher()
-    test.test_string_to_int_and_int_to_string()
 
-    print("Passed all tests")
+    run_tests = True
 
+    if run_tests:
+        test.test_extended_euclidean_algorithm()
+        test.test_power_mod()
+        test.test_int_to_list_and_list_to_int()
+        test.test_rabin_cipher()
+        test.test_string_to_int_and_int_to_string()
+
+        print("Passed all tests")
 if __name__ == '__main__':
     main()
